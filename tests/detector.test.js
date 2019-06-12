3f1659c6615edc3a86a687b7ad70fc7ef9e801bc
GermanBluefox <dogafox@gmail.com>
2019-06-12 23:03:39 +0200

    (bluefox) add tests

--- tests/detector.test.js
+++ tests/detector.test.js
@@ -3,12 +3,17 @@ var expect = require('chai').expect;
 const {Types, ChannelDetector} = require('../index');
 
 describe('Test Detector', () => {
-    it('Must detect temperature sensor', done => {
+    it('Must detect temperature sensor from channel', done => {
         const detector = new ChannelDetector();
 
         const objects = {
+            "ham.0.TemperatureAndHumidity" :{
+                "common": {
+                    "name": "Current Relative Humidity Channel",
+                },
+                "type": "channel"
+            },
             "ham.0.TemperatureAndHumidity.Current-Relative-Humidity" :{
-                "_id": "ham.0.TemperatureAndHumidity.Current-Relative-Humidity",
                 "common": {
                     "name": "Current Relative Humidity",
                     "type": "number",
@@ -22,6 +27,44 @@ describe('Test Detector', () => {
                 "type": "state"
             }
         };
+
+        Object.keys(objects).forEach(id => objects[id]._id = id);
+
+        const options = {
+            objects:            objects,
+            id:                 'ham.0.TemperatureAndHumidity',
+            _keysOptional:      Object.keys(objects),
+            _usedIdsOptional:   []
+        };
+
+        const controls = detector.detect(options);
+
+        console.log(JSON.stringify(controls));
+        expect(controls[0].type).to.be.equal('info');
+
+        done();
+    });
+
+    it('Must detect temperature sensor from state', done => {
+        const detector = new ChannelDetector();
+
+        const objects = {
+            "ham.0.TemperatureAndHumidity.Current-Relative-Humidity" :{
+                "common": {
+                    "name": "Current Relative Humidity",
+                    "type": "number",
+                    "unit": "%",
+                    "role": "value.humidity",
+                    "min": 0,
+                    "max": 100,
+                    "read": true,
+                    "write": false
+                },
+                "type": "state"
+            }
+        };
+
+        Object.keys(objects).forEach(id => objects[id]._id = id);
 
         const options = {
             objects:            objects,
