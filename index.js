4e9aeb96d18a54c3587bcea3e50d330e0c84635a
Achim Königs <garfonso@mobo.info>
2020-06-20 14:34:38 +0200

    allow role button.blind.stop

--- index.js
+++ index.js
@@ -366,7 +366,7 @@ function ChannelDetector() {
                 {role: /^level(\.blind)?$/,                   indicator: false, type: 'number',  write: true, enums: roleOrEnumBlind, name: 'SET',                 required: true, defaultRole: 'level.blind', defaultUnit: '%'},
                 // optional
                 {role: /^value(\.blind)?$/,                   indicator: false, type: 'number',               enums: roleOrEnumBlind, name: 'ACTUAL',              required: false, defaultRole: 'value.blind', defaultUnit: '%'},
-                {role: /^button\.stop$|^action\.stop$/,       indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: false, noSubscribe: true, defaultRole: 'button.stop'},
+                {role: /^button(\.blind)?\.stop$|^action\.stop$/,       indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: false, noSubscribe: true, defaultRole: 'button.blind.stop'},
                 {role: /^button(\.blind)?\.open$/,            indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'OPEN',                required: false, noSubscribe: true, defaultRole: 'button.blind.open'},
                 {role: /^button(\.blind)?\.close$/,           indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'CLOSE',               required: false, noSubscribe: true, defaultRole: 'button.blind.close'},
                 patternDirection,
@@ -389,7 +389,7 @@ function ChannelDetector() {
                 // optional
                 {role: /^value\.open\.blind$/,                indicator: false, type: 'number',               enums: roleOrEnumBlind, name: 'ACTUAL_OPEN',         required: false, defaultRole: 'value.open.blind'},
                 {role: /^value\.close\.blind$/,               indicator: false, type: 'number',               enums: roleOrEnumBlind, name: 'ACTUAL_CLOSE',        required: false, defaultRole: 'value.close.blind'},
-                {role: /^button\.stop$|^action\.stop$/,       indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: false, noSubscribe: true, defaultRole: 'button.stop'},
+                {role: /^button(\.blind)?\.stop$|^action\.stop$/,       indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: false, noSubscribe: true, defaultRole: 'button.blind.stop'},
                 {role: /^button(\.blind)?\.open$/,            indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'OPEN',                required: false, noSubscribe: true, defaultRole: 'button.blind.open'},
                 {role: /^button(\.blind)?\.close$/,           indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'CLOSE',               required: false, noSubscribe: true, defaultRole: 'button.blind.close'},
                 {role: /^level(\.open)?\.tilt$/,              indicator: false, type: 'number',  write: true, enums: roleOrEnumBlind, name: 'TILT_OPEN_SET',       required: false, defaultRole: 'level.open.tilt'},
@@ -411,9 +411,10 @@ function ChannelDetector() {
         blindButtons: {
             states: [
                 //blinds with no percentage setting / reading but buttons for up/down and stop:
-                {role: /^button\.stop$|^action\.stop$/,       indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: true,  noSubscribe: true, defaultRole: 'button.stop'},
+                {role: /^button(\.blind)?\.stop$|^action\.stop$/, indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'STOP',                required: true,  noSubscribe: true, defaultRole: 'button.blind.stop'},
                 {role: /^button(\.blind)?\.open$/,            indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'OPEN',                required: true,  noSubscribe: true, defaultRole: 'button.blind.open'},
                 {role: /^button(\.blind)?\.close$/,           indicator: false, type: 'boolean', write: true, enums: roleOrEnumBlind, name: 'CLOSE',               required: true,  noSubscribe: true, defaultRole: 'button.blind.close'},
+                //optional tilt:
                 {role: /^level(\.open)?\.tilt$/,              indicator: false, type: 'number',  write: true, enums: roleOrEnumBlind, name: 'TILT_OPEN_SET',       required: false, defaultRole: 'level.open.tilt'},
                 {role: /^level\.close\.tilt$/,                indicator: false, type: 'number',  write: true, enums: roleOrEnumBlind, name: 'TILT_CLOSE_SET',      required: false, defaultRole: 'level.close.tilt'},
                 {role: /^value(\.open)?\.tilt$/,              indicator: false, type: 'number',               enums: roleOrEnumBlind, name: 'TILT_OPEN_ACTUAL',    required: false, defaultRole: 'value.open.tilt'},
