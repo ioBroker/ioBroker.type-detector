827f24bada70530dd91a20e05f25ece7175e964c
GermanBluefox <dogafox@gmail.com>
2023-10-24 02:59:10 +0200

    Implemented as typescript

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 const pack = require('./package.json');
 const fs = require('fs');
+
 pack.name = '@iobroker/type-detector';
 delete pack.devDependencies;
 delete pack.scripts;
 fs.writeFileSync(`${__dirname}/build/package.json`, JSON.stringify(pack, null, 2));
+
+fs.writeFileSync(`${__dirname}/build/README.md`, f.readFileSync(`${__dirname}/README.md`));
+fs.writeFileSync(`${__dirname}/build/LICENSE`, f.readFileSync(`${__dirname}/LICENSE`));
